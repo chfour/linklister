d636f655d97b9e5309c233bd5a25abedd5e2ae2a
Michał Litwin <michaleklitwin@gmail.com>
2021-07-28 18:39:11 +0200

    added a custom useragent option and a quiet flag

--- linklister.py
+++ linklister.py
@@ -2,20 +2,20 @@
 import argparse, sys, requests
 from bs4 import BeautifulSoup
 
-USER_AGENT = "linklister"
+DEFAULT_USERAGENT = "linklister <github.com/chfour/linklister>"
 
-def recurse_find(url: str, file, verbose=False):
-    r = requests.head(url, headers={"user-agent": USER_AGENT})
+def recurse_find(url: str, file, verbose=False, user_agent=DEFAULT_USERAGENT):
+    r = requests.head(url, headers={"User-Agent": user_agent})
     if verbose: print(f"* HEAD {url} | code {r.status_code} type {r.headers.get('content-type', 'unknown')!r}", file=sys.stderr)
     if not r.ok: return
     file.write(f"{url}\n")
 
     if r.headers.get("content-type", None).startswith("text/html"):
-        r = requests.get(url, headers={"user-agent": USER_AGENT})
+        r = requests.get(url, headers={"User-Agent": user_agent})
         if verbose: print(f"* GET {url} | code {r.status_code}", file=sys.stderr)
         soup = BeautifulSoup(r.text, features="html5lib")
         links = soup.find_all("a")
-        print(f"* {len(links)} <a> tags")
+        if verbose: print(f"* {len(links)} <a> tags", file=sys.stderr)
         for link in links:
             if link["href"] in ["../", ".."]: continue
             recurse_find(url + link["href"], file, verbose)
@@ -26,6 +26,10 @@ if __name__ == "__main__":
                         help="URL to start from")
     parser.add_argument("-o", "--output", type=str, default="-",
                         help="output file to write to, - for stdout, default -")
+    parser.add_argument("-q", "--quiet", action="store_true",
+                        help="stay quiet and don't output anything to stderr")
+    parser.add_argument("-u", "--user-agent", type=str, default=DEFAULT_USERAGENT,
+                        help=f"set custom user agent, default is '{DEFAULT_USERAGENT}'")
     args = parser.parse_args()
 
     if args.output == "-":
@@ -34,4 +38,4 @@ if __name__ == "__main__":
         output = open(args.output, "w")
 
     with output:
-        recurse_find(args.start_url, output, verbose=True)
+        recurse_find(args.start_url, output, verbose=(not args.quiet), user_agent=args.user_agent)
